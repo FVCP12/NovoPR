33c608b501f0669f485fae0736a677855d394808
Felipe <fvcp12@gmail.com>
2019-02-13 23:37:38 -0200

    Teste 3d

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Dimensions } from 'react-native';
 
 
 export default class App extends React.Component {
   render() {
     return (
+      /* Fazendo teste com transform */
       <View style={styles.container}>
-        <Text>Esse é um teste com o react native, o meu nome é Felipe!</Text>
-        <Text>Teste com git</Text>
+        <View style={styles.child}/>
       </View> 
     );
   }
@@ -16,8 +16,17 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'green',
+  },
+  child: {
+    flex: 1,
+    backgroundColor: "blue",
+    transform: [
+        {perspective: 850}, //parece que nao tem muita importacia no Android (feito teste no emulador online
+        // e o android sem esse objeto funcionou normalmente, no ios deu erro ficou apenas um tira azul 
+        //sem o efeito de 3d)
+        {translateX: - Dimensions.get('window').width * 0.24}, // move o objeto no eixo x 
+        {rotateY: '60deg'}, // rotaciona o objeto nno eixo y
+    ],
   },
 });
